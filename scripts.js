75760d583d4d84dcf29aac1a67cc55f5f1274046
SibusisoNgubeni <156700279+SibusisoNgubeni@users.noreply.github.com>
2024-05-23 21:49:00 +0200

    Sorted provinces function update

--- scripts.js
+++ scripts.js
@@ -5,6 +5,7 @@ const provinces = ['Western Cape', 'Gauteng', 'Northern Cape', 'Eastern Cape', '
 const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikkie']
 
 // A list of products with prices:
+
 const products = [
   { product: 'banana', price: "2" },
   { product: 'mango', price: 6 },
@@ -13,3 +14,34 @@ const products = [
   { product: 'coffee', price: 10 },
   { product: 'tea', price: '' },
 ]
+
+
+
+
+provinces.forEach((province, index) => {
+    const name = names[index];
+    console.log(` ${name} (${province})`);
+ });
+  
+const upperCase = () => {
+ const upperCaseProvinces = provinces.map((province) => 
+  province.toUpperCase())
+ console.log(upperCaseProvinces)
+}
+ upperCase();
+
+ const lengths = names.map((name) => name.length)
+ console.log(lengths);
+
+
+ /*this function creates a new array with the province names converted to lowercase using the map method
+ because the default 'sort' behavior is case-sensitive (i.e., uppercase letters come before lowercase letters).
+ */
+ const sortedProvinces =  provinces.map((province) => 
+    province.toLowerCase()).sort();
+
+ console.log(sortedProvinces);
+  
+ 
+
+ 
